61afd1b553cc29d2a4b3826ed1e75516d27cfdd2
Thayaan1998 <selvathayaan3@gmail.com>
2022-03-13 09:17:35 +0530

    Third commit

--- src/main/java/com/classproject/classprojectbackend/service/UserService.java
+++ src/main/java/com/classproject/classprojectbackend/service/UserService.java
@@ -17,7 +17,6 @@ public class UserService {
 
         return  userRepository.save(user);
     }
-    private String name;
 
     public Users getUserByEmail(String email){
         return userRepository.getUserByEmail(email);
